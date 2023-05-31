cb0ea9433a277c39ea67c9a83224b5eb0060cd67
Doffel <syaidahmad7z@gmail.com>
2023-05-31 14:09:05 +0700

    Update

--- detail.php
+++ detail.php
@@ -436,9 +436,50 @@
     </div>
     <!-- Shop Detail End -->
 
-
+    <?php $kategori = $result['kategori']; 
+    $queryRekomendasi = mysqli_query($connect, "SELECT * FROM databarang WHERE kategori='$kategori' AND idbarang <> '$idbarang' LIMIT 4");
+    ?>
     <!-- Products Start -->
     <div class="container-fluid py-5">
+        <div class="text-center mb-4">
+            <h2 class="section-title px-5"><span class="px-2">Kamu Mungkin Suka</span></h2>
+        </div>
+        <div class="row px-xl-5">
+            <div class="col">
+                <div class="owl-carousel related-carousel">
+                    <?php
+                    while ($row = mysqli_fetch_assoc($queryRekomendasi)) {
+                        $gambarRekomendasi = $row['idbarang'] . ".png";
+                        $gambarPathRekomendasi = "produk/" . $gambarRekomendasi;
+                        $hargas = number_format($row['hargabarang'], 0, ',', '.');
+                        $idbarangs = $row['idbarang'];
+                    ?>
+                    <div class="card product-item border-0">
+                        <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                            <img class="img-fluid w-100" src="<?php echo $gambarPathRekomendasi; ?>" alt="">
+                        </div>
+                        <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
+                            <h6 class="text-truncate mb-3"><?php echo $row['namabarang']; ?></h6>
+                            <div class="d-flex justify-content-center">
+                                <h6>RP <?php echo $hargas; ?></h6>
+                            </div>
+                        </div>
+                        <div class="card-footer d-flex justify-content-between bg-light border">
+                            <?php
+                            echo '<a href="detail.php?id='.$idbarangs.'" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>View Detail</a>';
+                            echo '<a href="#" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>';
+                            ?>
+                        </div>
+                    </div>
+                    <?php
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="container-fluid py-5">
         <div class="text-center mb-4">
             <h2 class="section-title px-5"><span class="px-2">Kamu Mungkin Suka</span></h2>
         </div>
@@ -475,6 +516,10 @@
                             <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
                         </div>
                     </div>
+                    <div class="card product-item border-0">
+                        <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                            <img class="img-fluid w-100" src="img/cat-1.jpg" alt="">
+                        </div>
                         <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
                             <h6 class="text-truncate mb-3">Colorful Stylish Shirt</h6>
                             <div class="d-flex justify-content-center">
@@ -486,10 +531,11 @@
                             <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
                         </div>
                     </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
     <!-- Products End -->
 
 
